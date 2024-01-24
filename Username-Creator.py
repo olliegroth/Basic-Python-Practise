83a75bec5975d983c6924a53b31914fdff025150
= <=>
2024-01-24 18:53:41 +0000

    Improved Username-Creator.py

--- Username-Creator.py
+++ Username-Creator.py
@@ -1,22 +1,17 @@
-def ask():
-    more = input("Do you want to make more usernames? ")
-    if more.lower() == "yes":
+import re
+
+def username():
+    try:
+        name = str(input("What is your name? "))
+        splitName = name.split(" ")
+        generatedUsername = f'{str(splitName[0])}.{str(splitName[1])}'
+        print(f'Your username is: {generatedUsername}')
+    except: # Use regex to make sure users enter their name in name (space) surname format
+        print("Please use a space in your name\n")
+
+while True:
+    doYouWantUsername = input("Do you want to make (another) a username? ")
+    if doYouWantUsername.lower() == "yes" or doYouWantUsername.lower() == "y":
         username()
     else:
-        x = False
-
-x = True
-while x == True:
-    def username():
-        try:
-            name = str(input("What is your name? "))
-            n = name.split(" ")
-            first = n[0]
-            last = n[1]
-            username = (str(first) + str(last))
-            print("Your username is: "+username)
-            ask()
-            x = False
-        except:
-            print("Please use a space in your name\n")
-    username()
+        exit()
